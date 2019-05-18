fc7580e20143d2259c034acb34ebf78019482c54
DerzkeyBK <43850433+DerzkeyBK@users.noreply.github.com>
2019-05-18 10:46:32 +0300

    Разделил back и front

--- HackHackHack/HackHackHack/Program.cs
+++ HackHackHack/HackHackHack/Program.cs
@@ -17,6 +17,7 @@ namespace HackHackHack
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
